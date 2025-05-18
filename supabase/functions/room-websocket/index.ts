1edc45a1ff578660663167027e365d259201320f
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2025-05-18 15:37:25 +0200

    feat: fetch round info and change dish_round column name

--- supabase/functions/room-websocket/index.ts
+++ supabase/functions/room-websocket/index.ts
@@ -5,23 +5,14 @@
 // Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { createClient } from 'jsr:@supabase/supabase-js@2'
+import { Order, Round } from './interfaces.ts'
 
 const supabase = createClient(
   Deno.env.get("APP_SUPABASE_URL"),
   Deno.env.get("APP_SUPABASE_SERVICE_ROLE_KEY"),
 );
 
-Deno.serve(async (req: any) => {
-  // Check that request is a websocket upgrade
-  const upgrade = req.headers.get("upgrade") || "";
-  if (upgrade.toLowerCase() != "websocket") {
-    return new Response("request isn't trying to upgrade to websocket.", { status: 400 });
-  }
-
-  // Check that order exists and is opened
-  const url = new URL(req.url);
-  const order_uuid = url.searchParams.get('order_uuid');
-
+const fetchOrder = async (order_uuid: string): Promise<Order | Response> => {
   const { data , error } = await supabase
     .from('orders')
     .select('*')
@@ -30,26 +21,69 @@ Deno.serve(async (req: any) => {
     .limit(1);
 
   if (error) {
-    console.error("Error checking order:", error);
     return new Response("Error checking order.", { status: 400 });
   }
 
   if (!data || data.length === 0) {
-    console.error("Order not found or not open.");
     return new Response("Order not found or not open.", { status: 400 });
   }
 
-  // Upgrade request to websocket and handle it
+  return data[0];
+};
+
+const fetchRound = async (order_id: number): Promise<Round | Response> => {
+  const { data, error } = await supabase
+  .from('rounds')
+  .select('number, is_open, dishes (id, name, dish_round (quantity))')
+  .eq('order_id', order_id)
+  .eq('is_open', true)
+  .limit(1)
+  .single();
+
+  if (error) {
+    return new Response("Error fetching round.", { status: 500 });
+  }
+
+  if (!data) {
+    return new Response("Round not found.", { status: 400 });
+  }
+
+  return data;
+}
+
+Deno.serve(async (req: any) => {
+  // Check that request is a websocket upgrade
+  const upgrade = req.headers.get("upgrade") || "";
+  if (upgrade.toLowerCase() != "websocket") {
+    return new Response("request isn't trying to upgrade to websocket.", { status: 400 });
+  }
+
+  // Get order_uuid from URL and check that it exists
+  const url = new URL(req.url);
+  const order_uuid = url.searchParams.get('order_uuid');
+  if (!order_uuid) {
+    return new Response("Missing order_uuid", { status: 400 });
+  }
+
+  const order = await fetchOrder(order_uuid);
+  if (order instanceof Response) {
+    return order;
+  }
+
+  // Upgrade request to websocket, fetch round and manage websocket connection
   const { socket, response } = Deno.upgradeWebSocket(req);
 
+  const round = await fetchRound(order.id);
+  if (round instanceof Response) {
+    return round;
+  }
+
   socket.onopen = () => {
-    console.log("client connected!");
     socket.send(`Welcome to Plateroom for order ${order_uuid}.`);
-    socket.send(`Order data: ${JSON.stringify(data)}`);
+    socket.send(`Round data: ${JSON.stringify(round)}`);
   };
   
   socket.onmessage = (e: any) => {
-    console.log("client sent message:", e.data);
     socket.send(`Server received: ${e.data}`);
   };
 
---
+++ supabase/migrations/20250518123722_rename_dish_round_amount_to_quantity.sql
@@ -0,0 +1,1 @@
+ALTER TABLE public.dish_round RENAME COLUMN amount TO quantity;
